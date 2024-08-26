80761cf12a2c060f406af4589d92d0fe00a8954e
devops39 <voulverine_22@hotmail.com>
2024-08-26 12:54:26 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/Dockerfile

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -22,14 +22,26 @@ resource "aws_api_gateway_integration" "lambda_integration" {
   http_method             = aws_api_gateway_method.method.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.todo_app.invoke_arn
+  uri                     = "${aws_lambda_function.todo_app.invoke_arn}"
+
+  integration_response {
+    status_code = "200"
+
+    response_templates = {
+      "application/json" = <<EOF
+        {
+          "message": "Integration response"
+        }
+        EOF
+    }
+  }
 }
 
 resource "aws_api_gateway_deployment" "deployment" {
+  depends_on = [aws_api_gateway_integration.lambda_integration]
   rest_api_id = aws_api_gateway_rest_api.todo_api.id
-  stage_name  = "prod
+  stage_name  = "prod"
 }
-
 
 output "api_gateway_url" {
   value = aws_api_gateway_deployment.deployment.invoke_url
