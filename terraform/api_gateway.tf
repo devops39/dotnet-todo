19d25362e48323d0aa6ab1b22ae464c9ea809d6e
devops39 <voulverine_22@hotmail.com>
2024-08-26 12:40:05 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/Dockerfile

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -28,6 +28,7 @@ resource "aws_api_gateway_integration" "lambda_integration" {
 resource "aws_api_gateway_deployment" "deployment" {
   rest_api_id = aws_api_gateway_rest_api.todo_api.id
   stage_name  = "prod
+}
 
 output "api_gateway_url" {
   value = aws_api_gateway_deployment.deployment.invoke_url
