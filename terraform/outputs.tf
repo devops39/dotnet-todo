41636b30d89f5ab0c3b7ab625ed2f33fded23e9e
devops39 <voulverine_22@hotmail.com>
2024-08-26 14:02:25 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/Dockerfile

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,7 +2,7 @@ output "lambda_function_name" {
   value = aws_lambda_function.todo_app.function_name
 }
 
-output "api_url" {
+output "api_gateway_url" {
   value = aws_api_gateway_deployment.todo_deployment.invoke_url
 }
 
