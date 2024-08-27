989716ef899aa2c0bb760ad792d781bd8e06feb7
devops39 <voulverine_22@hotmail.com>
2024-08-27 11:44:12 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/Dockerfile

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_role" "lambda_role" {
+resource "aws_iam_role" "itadmin" {
   name = "lambda-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -13,13 +13,13 @@ resource "aws_iam_role" "lambda_role" {
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
-  role       = aws_iam_role.lambda_role.name
+  role       = aws_iam_role.itadmin.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
 resource "aws_lambda_function" "todo_app" {
   function_name = "todo-app-lambda"
-  role          = aws_iam_role.lambda_role.arn
+  role          = aws_iam_role.itadmin.arn
   handler       = "app.lambda_handler"
   runtime       = "python3.8"  # Change to your application runtime
   image_uri     = "docker.io/devoops39/dotnet-todo:latest"  # Path to your packaged Lambda deployment file
