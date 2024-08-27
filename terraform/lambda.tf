4f577275f80a15bd73d23027328498c65fbf4802
devops39 <voulverine_22@hotmail.com>
2024-08-27 15:30:42 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/Dockerfile

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -23,7 +23,7 @@ resource "aws_lambda_function" "todo_app" {
   handler       = "app.lambda_handler"      # Update with the correct handler for your function
   runtime       = "python3.8"               # Update with the correct runtime for your function
   s3_bucket     = "myappbucket99"     # Replace with your S3 bucket name
-  s3_key        = "s3://myappbucket99/dotnet-todo.zip"  # Replace with the S3 key (path) to your ZIP file
+  s3_key        = "dotnet-todo.zip"  # Replace with the S3 key (path) to your ZIP file
 
   vpc_config {
     subnet_ids         = data.aws_subnets.default.ids
