766b19085e122b164a2039c20fcb3e90e2160d69
devops39 <voulverine_22@hotmail.com>
2024-08-27 15:28:00 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/Dockerfile

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -20,8 +20,10 @@ resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
 resource "aws_lambda_function" "todo_app" {
   function_name = "todo-app-lambda"
   role          = aws_iam_role.terraadmin.arn
-  package_type  = "Image"
-  image_uri     = "docker.io/devoops39/dotnet-todo:latest"  # Path to your packaged Lambda deployment file
+  handler       = "app.lambda_handler"      # Update with the correct handler for your function
+  runtime       = "python3.8"               # Update with the correct runtime for your function
+  s3_bucket     = "myappbucket99"     # Replace with your S3 bucket name
+  s3_key        = "s3://myappbucket99/dotnet-todo.zip"  # Replace with the S3 key (path) to your ZIP file
 
   vpc_config {
     subnet_ids         = data.aws_subnets.default.ids
