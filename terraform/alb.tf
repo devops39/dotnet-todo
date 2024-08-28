5669112185139094ec7524c1fde750b711cd6793
devops39 <voulverine_22@hotmail.com>
2024-08-28 16:35:40 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/Dockerfile

---
+++ terraform/alb.tf
@@ -0,0 +1,43 @@
+resource "aws_lb" "app_lb" {
+  name               = "app-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb_sg.id]
+  subnets            = data.aws_subnets.default.ids
+}
+
+resource "aws_lb_target_group" "lambda_tg" {
+  name        = "lambda-tg"
+  target_type = "lambda"
+  vpc_id      = data.aws_vpc.default.id
+}
+
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.lambda_tg.arn
+  }
+}
+
+resource "aws_lb_listener_rule" "route_todoitems" {
+  listener_arn = aws_lb_listener.http_listener.arn
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.lambda_tg.arn
+  }
+  condition {
+    field  = "path-pattern"
+    values = ["/todoitems*", "/todoitems/{id}"]
+  }
+}
+
+resource "aws_lambda_permission" "alb_lambda" {
+  statement_id  = "AllowExecutionFromALB"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.todo_app.function_name
+  principal     = "elasticloadbalancing.amazonaws.com"
+  source_arn    = aws_lb_target_group.lambda_tg.arn
+}
