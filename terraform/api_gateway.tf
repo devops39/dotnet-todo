72c8eb8ab9045946af56bdd5550360c056ab0612
devops39 <voulverine_22@hotmail.com>
2024-08-28 15:37:58 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/Dockerfile

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -18,6 +18,46 @@ resource "aws_api_gateway_method" "method" {
   authorization = "NONE"
 }
 
+resource "aws_api_gateway_method" "get_todoitems" {
+  rest_api_id   = aws_api_gateway_rest_api.todo_api.id
+  resource_id   = aws_api_gateway_resource.resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "get_todoitems_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.todo_api.id
+  resource_id             = aws_api_gateway_resource.resource.id
+  http_method             = aws_api_gateway_method.get_todoitems.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.todo_app.invoke_arn
+}
+
+# Additional resource and method for /todoitems/{id}
+resource "aws_api_gateway_resource" "todoitem_id" {
+  rest_api_id = aws_api_gateway_rest_api.todo_api.id
+  parent_id   = aws_api_gateway_resource.resource.id
+  path_part   = "{id}"
+}
+
+resource "aws_api_gateway_method" "get_todoitem_by_id" {
+  rest_api_id   = aws_api_gateway_rest_api.todo_api.id
+  resource_id   = aws_api_gateway_resource.todoitem_id.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "get_todoitem_by_id_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.todo_api.id
+  resource_id             = aws_api_gateway_resource.todoitem_id.id
+  http_method             = aws_api_gateway_method.get_todoitem_by_id.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.todo_app.invoke_arn
+}
+
+
 resource "aws_api_gateway_integration" "lambda_integration" {
   rest_api_id             = aws_api_gateway_rest_api.todo_api.id
   resource_id             = aws_api_gateway_resource.resource.id
@@ -28,9 +68,14 @@ resource "aws_api_gateway_integration" "lambda_integration" {
 }
 
 resource "aws_api_gateway_deployment" "todo_deployment" {
-  depends_on  = [aws_api_gateway_integration.lambda_integration]
+  depends_on  = [
+    aws_api_gateway_integration.lambda_integration,
+    aws_api_gateway_integration.get_todoitems_integration,
+    aws_api_gateway_integration.get_todoitem_by_id_integration
+  ]
   rest_api_id = aws_api_gateway_rest_api.todo_api.id
   stage_name  = "prod"
 }
+
 
 
